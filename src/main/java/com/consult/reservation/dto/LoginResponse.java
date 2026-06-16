f497f85033323fd58f036b08e3f8b027741338e3
idy0327 <idy0327@fermi.co.kr>
2026-06-17 08:34:13 +0900

    feat : 로그인 응답시 role 추가`

--- src/main/java/com/consult/reservation/dto/LoginResponse.java
+++ src/main/java/com/consult/reservation/dto/LoginResponse.java
@@ -12,9 +12,11 @@ public class LoginResponse {
 
     private final Long id;
     private final String name;
+    private final String role;
 
     public LoginResponse(User user) {
         this.id = user.getId();
         this.name = user.getName();
+        this.role = user.getRole();
     }
 }
